5e16458dc058a79db69317cc7cd229a565fb47e2
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-25 13:45:15 -0400

    add email checking functionality to form

--- src/components/PodcastForm/PodcastForm.js
+++ src/components/PodcastForm/PodcastForm.js
@@ -16,6 +16,32 @@ function define(html) {
     // VARIABLES
 
     // METHODS
+    handleSubmit(e, inputEmail, errorMessage) {
+      // regex for checking valid email
+      const validEmail =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      const isValid = inputEmail.value.match(validEmail);
+
+      if (!isValid) {
+        // prevent form from submitting if there's an error
+        e.preventDefault();
+        errorMessage.classList.remove('hide');
+      } else {
+        errorMessage.classList.add('hide');
+      }
+    }
+
+    // LIFE CYCLE METHODS
+    connectedCallback() {
+      const form = this.shadowRoot.lastElementChild.children[2];
+      const inputEmail = form.firstElementChild.children[1];
+      const errorMessage = form.firstElementChild.children[2];
+
+      form.addEventListener('submit', (e) =>
+        this.handleSubmit(e, inputEmail, errorMessage)
+      );
+    }
   }
   window.customElements.define('podcast_form-component', PodcastForm);
 }
